4d0cecd1ec58adb8987c7f9c43bbb78ab8283917
清水 智秀 <33281797+tomohideshimizu@users.noreply.github.com>
2017-11-01 19:41:38 +0900

    1
    1

---
+++ rdp_autologin_custom_resolution.bat
@@ -0,0 +1,10 @@
+﻿@Echo off
+
+Set SERVER=server:3389
+set WIDTH=1920
+set HEIGHT=1080
+Set USERNAME=username
+Set PASSWORD=password
+
+Cmdkey /generic:TERMSRV/%SERVER% /user:%USERNAME% /pass:%PASSWORD%
+Start mstsc /v:%SERVER% /w:%WIDTH% /h:%HEIGHT%
